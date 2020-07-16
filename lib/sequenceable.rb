95d49520b0789a42ed0557b61fa9c3b4b70c0517
Sandip Mane <sandip2490@gmail.com>
2020-07-16 21:29:12 +0530

    Fixed an issue with schema load

--- lib/sequenceable.rb
+++ lib/sequenceable.rb
@@ -12,10 +12,6 @@ module Sequenceable
     included_modules.include?(Sequenceable::Core)
   end
 
-  def has_valid_sequenceable_column?(column_name)
-    [:integer, :float, :bigint, :decimal].include? self.column_for_attribute(column_name).type
-  end
-
   def acts_in_sequence(options = {})
     raise ArgumentError, "acts_in_sequence => Hash expected, got #{options.class.name}" if !options.is_a?(Hash)
 
@@ -29,10 +25,6 @@ module Sequenceable
 
     if options[:column_name].present?
       sequencing_configuration[:column_name] = options[:column_name]
-    end
-
-    if !has_valid_sequenceable_column?(sequencing_configuration[:column_name])
-      raise ArgumentError, "acts_in_sequence => Column `#{sequencing_configuration[:column_name]}` needs to be of type `:integer`"
     end
 
     if options[:default_order].present?
